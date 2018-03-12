82312f475c66b5244bec0d25c3f59de87fb65b38
ThorEFW <Thorwetche@gmail.com>
2018-03-12 12:44:32 +0100

    Newest Update

--- StartPage.py
+++ StartPage.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 #   ********************* Variables
 
@@ -6,7 +7,9 @@ HEADLINE_FONT = ("Times",18)
 LARGE_FONT = ("Times", 12)
 Bg_theme = ("#1e353e")
 B_color = ("#1e73f7")
-global uName
+uName = ("Thor")
+global TOP
+#RobotImg = tk.PhotoImage(file="Robot.png")
 
 
 #   ********************* Baseline! *************************
@@ -29,6 +32,7 @@ class Page(tk.Tk):
         fileMenu = tk.Menu(self)
         mainMenu.add_cascade(label="File", menu=fileMenu)
         fileMenu.add_command(label="Sign Out", command=lambda: self.show_frame(Log_In)) #This uses
+        fileMenu.add_command(label="Start Page", command=lambda: self.show_frame(StartPage))
         fileMenu.add_command(label="Quit", command=quit)
 
         statusBar = tk.Label(self, text="Nothing", bg=Bg_theme, bd=2)  # bd=border, creates a border, relief makes it looked sunken in the screen as when a button is pressed
@@ -62,7 +66,7 @@ class Page(tk.Tk):
 
 
 
-        self.show_frame(Log_In)
+        self.show_frame(RobotControl)
 
     def show_frame(self, cont):     #the "container" or "Controller" is the key whcih will look for the value of "self.frame"
                                     #   The key will specify a frame, which will be raised to the front in the "show_frame"-function
@@ -71,9 +75,9 @@ class Page(tk.Tk):
 
 
 
+
 #   This class will inherit from tk.frame
 class Log_In(tk.Frame):
-
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg=Bg_theme, width=400, padx=370, pady=150)
@@ -93,15 +97,16 @@ class Log_In(tk.Frame):
         pass_entry = tk.Entry(self, width=25)
         pass_entry.grid(row=3, column=1, sticky="w")
 
-        #self.name = name_entry.get()
+        uName = name_entry.get()
 
-
-        log_but = tk.Button(self, text="Log In", width=16,bg=B_color, fg="white",
-                            command=lambda: controller.show_frame(StartPage))
+        log_but = ttk.Button(self, text="Log In", width=16,
+                             command=lambda: controller.show_frame(StartPage))
         log_but.grid(row=4, column=0, pady=7)
+       # log_but.bind("<Button-1>", lambda: controller.show_frame(StartPage))
 
         q_but = tk.Button(self, text="Quit", width=16, bg=B_color, fg="white")
         q_but.grid(row=4, column=1, sticky="e")
+
 
 
 class StartPage(tk.Frame):
@@ -109,7 +114,7 @@ class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg=Bg_theme)
 
-        label_Head = tk.Label(self, text="Welcome to the Multi-tool!" , font= HEADLINE_FONT, bg=Bg_theme,fg="white")
+        label_Head = tk.Label(self, text="Welcome to the Multi-tool "+uName+"!", font= HEADLINE_FONT, bg=Bg_theme,fg="white")
         label_intro = tk.Label(self, text=" Below you will se a button where you \n can choose what ever tool you want!",bg=Bg_theme,fg="white")
         label_intro2 = tk.Label(self, text="Amazing, i know...",bg=Bg_theme,fg="white")
         label_intro3 = tk.Label(self, text="The current tools are the following: ", bg=Bg_theme, fg="white")
@@ -127,31 +132,58 @@ class StartPage(tk.Frame):
         button_movie.pack()
 
 
+
 class RobotControl(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg=Bg_theme)
-        label = tk.Label(self, text="ControlPanel", font=LARGE_FONT)   #self is root
-        label.pack(pady=5, padx=5)
 
-        but1 = tk.Button(self, text="Button")
-        #but.grid(row=2, column=3)
-        but1.pack()
+        HeadLineFrame = tk.Frame(self, bg=Bg_theme)
+        HeadLineFrame.grid(row=0, column=0, sticky="EW")
 
-        but2 = tk.Button(self, text="Button 2")
-        #but.grid(row=3, column=5)
-        but2.pack()
+        label1 = tk.Label(HeadLineFrame, text="HEY")
+        label1.grid(row=0, column=0)
 
-        button2 = tk.Button(self, text="Go to StartPage!",
-                            command=lambda: controller.show_frame(StartPage))  # By using lambda i can use the function for many things.
-        button2.pack()
+        left_frame = tk.Frame(self, bg="blue")
+        left_frame.grid(row=1, column=0)
+
+        label2 = tk.Label(left_frame, text="HEY")
+        label2.grid(row=0, column=0)
+
+        right_frame = tk.Frame(self, bg="green")
+        right_frame.grid(row=2, column=0)
+
+        label3 = tk.Label(right_frame, text="HEY")
+        label3.grid(row=0, column=0)
+
+        #RobotImg = tk.PhotoImage(file="Robot.png")
+
+        #Head_Img = tk.Label(self, image=RobotImg)
+        #Head_Img.grid(row=0, column=0)
+
+        #label = tk.Label(self, text="ControlPanel", font=HEADLINE_FONT, bg=Bg_theme, fg="white")   #self is root
+        #label.grid(row=0, column=1, sticky="w")
+
+        #ut1 = tk.Button(self, text="Button")
+
+        #but1.grid(row=1, column=0)
+
+        #but2 = tk.Button(self, text="Button 2")
+
+        #ut2.grid(row=2, column=0)
+
+
+        #com_entry = tk.Entry(self, width=41)
+        #com_entry.grid(row=4, column=0)
+
+
 
 
 class MovieDecider(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, bg=Bg_theme)
-        label = tk.Label(self, text="Movie Decider!", font=LARGE_FONT)   #self is root
+        label = ttk.Label(self, text="Movie Decider!", font=LARGE_FONT)   #self is root
         label.pack(pady=5, padx=5)
 
 
