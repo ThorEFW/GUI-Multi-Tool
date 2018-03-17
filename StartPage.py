59bdd2838394e8457cb32f4523e85c79569a053b
GustavWetche <gustavfauerby.dk@gmail.com>
2018-03-18 00:11:54 +0100

    Updated StartPage and Pics

--- StartPage.py
+++ StartPage.py
@@ -78,34 +78,40 @@ class Log_In(tk.Frame):
 
     def __init__(self, parent, controller, start_page):
         self.start_page = start_page
-        tk.Frame.__init__(self, parent, bg=Bg_theme, width=400, padx=370, pady=150)
+        tk.Frame.__init__(self, parent)#, bg=Bg_theme, width=400, padx=370, pady=150)
+        self.Universe = tk.PhotoImage(file="Pics/Universe.png")
 
-        label_Head = tk.Label(self, text="Please state your name!", font=HEADLINE_FONT, bg=Bg_theme,fg="white")
+        self.PhotoLabel = tk.Label(self, image=self.Universe)
+        self.PhotoLabel.place(x=0, y=0, relwidth=1, relheight=1)
+
+        main_frame = tk.Frame(self, bg=Bg_theme)  # bg
+        main_frame.place(relx=0.5, rely=0.5, anchor="center")
+
+        label_Head = tk.Label(main_frame, text="Please state your name!", font=HEADLINE_FONT, bg=Bg_theme,fg="white")
         label_Head.grid(row=0, columnspan=2)     #(side='top', expand=True)
-        label_intro = tk.Label(self, text="Or you want get access to this wonderful tool", font=LARGE_FONT, bg=Bg_theme, fg="white")
+        label_intro = tk.Label(main_frame, text="Or you want get access to this wonderful tool", font=LARGE_FONT, bg=Bg_theme, fg="white")
         label_intro.grid(row=1, columnspan=5)        #(side='top',expand=True)
 
-        name_label = tk.Label(self, text="Name: ",font=LARGE_FONT, bg=Bg_theme, fg="white")
+        name_label = tk.Label(main_frame, text="Name: ",font=LARGE_FONT, bg=Bg_theme, fg="white")
         name_label.grid(row=2, column=0, sticky="w")         #(side='top',expand=True)
-        name_entry = tk.Entry(self,width=25)
+        name_entry = tk.Entry(main_frame,width=25)
         name_entry.grid(row=2, column=1, sticky="w")         #(side='top',expand=True)
 
-        pass_label = tk.Label(self, text="Password: ",font=LARGE_FONT, bg=Bg_theme, fg="white")
+        pass_label = tk.Label(main_frame, text="Password: ",font=LARGE_FONT, bg=Bg_theme, fg="white")
         pass_label.grid(row=3, column=0, sticky="w")
-        pass_entry = tk.Entry(self,show="*", width=25)
+        pass_entry = tk.Entry(main_frame,show="*", width=25)
         pass_entry.grid(row=3, column=1, sticky="w")
 
 
         #log_but = tk.Button(self, text="Log In", width=16,
         #                     command=lambda: controller.show_frame(StartPage))
-        log_but = tk.Button(self, text="Log In", width=16,
+        log_but = tk.Button(main_frame, text="Log In", width=16, bg=B_color,fg="white",
                             command=lambda: self.start_page.setName_S(name_entry.get(), pass_entry.get()))
         log_but.grid(row=4, column=0, pady=7)
        # log_but.bind("<Button-1>", lambda: controller.show_frame(StartPage))
 
-        q_but = tk.Button(self, text="Quit", width=16, bg=B_color, fg="white")
+        q_but = tk.Button(main_frame, text="Quit", width=16, bg=B_color, fg="white")
         q_but.grid(row=4, column=1, sticky="e")
-
 
 
 class StartPage(tk.Frame):
@@ -117,6 +123,11 @@ class StartPage(tk.Frame):
         self.controll = controll
         tk.Frame.__init__(self, parent, bg=Bg_theme)
         self.controller = controller
+
+        self.Universe = tk.PhotoImage(file="Pics/Universe.png")
+        self.PhotoLabel = tk.Label(self, image=self.Universe)
+        self.PhotoLabel.place(x=0, y=0, relwidth=1, relheight=1)
+
         self.label_Head = tk.Label(self, font=HEADLINE_FONT,
                                    bg=Bg_theme, fg="white")
         self.label_pass = tk.Label(self, bg=Bg_theme, fg=FG)
@@ -149,6 +160,8 @@ class StartPage(tk.Frame):
             self.label_Head.configure(text = "Welcome to the Multi-tool " + self.uname + "!")
         if passW != None:
             self.label_pass.configure(text="Your password is '"+self.passW+ "', hehe")
+        else:
+            self.label_pass.configure(text="You did not enter a password..")
         self.tkraise()
         return name
 
@@ -161,100 +174,110 @@ class RobotControl(tk.Frame):
 
         tk.Frame.__init__(self, parent, bg=Bg_theme)
 
+        self.Universe = tk.PhotoImage(file="Pics/Universe.png")
+
+        self.PhotoLabel = tk.Label(self, image=self.Universe)
+        self.PhotoLabel.place(x=0, y=0, relwidth=1, relheight=1)
+
         self.txtOut  = ""
-        self.RobotImg = tk.PhotoImage(file="Robot.png")
+        self.RobotImg = tk.PhotoImage(file="Pics/Robot.png")
+        self.WALLE = tk.PhotoImage(file="Pics/Walle.png")
     # --------------------------------------------------    Top
-        HeadLineFrame = tk.Frame(self, bg=Bg_theme)
-        HeadLineFrame.pack(side="top", fill="x")
+        #HeadLineFrame = tk.Frame(self, bg=Bg_theme)
+        #HeadLineFrame.pack(side="top", fill="x")
 
-        H_line = tk.Label(HeadLineFrame, text="HeyRobot Control Center", font=HEADLINE_FONT, bg=Bg_theme, fg="white")
-        self.fil_lab = tk.Label(HeadLineFrame, text="__________", bg=Bg_theme, fg=Bg_theme, width=23)
-        self.but_instruct = tk.Label(HeadLineFrame, text="Instruction Buttons", bg=Bg_theme, fg="white", font=LARGE_FONT2)
+        H_line = tk.Label(self, text="Robot Control Center", font=HEADLINE_FONT, bg=Bg_theme, fg="white")
+        self.fil_lab = tk.Label(self, text="__________", bg=Bg_theme, fg=Bg_theme, width=23)
+        self.but_instruct = tk.Label(self, text="Instruction Buttons", bg=Bg_theme, fg="white", font=LARGE_FONT2)
 
-        H_line.grid(row=0, column=1)
-        self.fil_lab.grid(row=0, column=2)
+        H_line.grid(row=0, columnspan=2)
         self.but_instruct.grid(row=0, column=3)
 
         #Head_Img = tk.Label(HeadLineFrame, image=RobotImg)
         #Head_Img.grid(row=0, column=0)
 
     # ----------------------------------------------------  Left
-        left_frame = tk.Frame(self, bg=Bg_theme)
-        left_frame.pack(side="left", fill="y")
+        #left_frame = tk.Frame(self, bg=Bg_theme)
+        #left_frame.pack(side="left", fill="y")
 
-        self.but_send = tk.Button(left_frame, text="Send", width=5, bg=BC, fg=FG)
-        self.mes_entry = tk.Entry(left_frame, width=40)
-        self.but_del = tk.Button(left_frame, text="Delete text", width=10, bg=BC, fg=FG)
-        self.txt_field = tk.Text(left_frame, width=45, height=22, wrap="word")
-        self.F_forward = tk.Button(left_frame, text="Fast Forward", width=25, bg=BC, fg=FG)
-        self.S_forward = tk.Button(left_frame, text="Slow Forward", width=25, bg=BC, fg=FG)
-        self.F_backward = tk.Button(left_frame, text="Fast Backward", width=25, bg=BC, fg=FG)
-        self.S_backward = tk.Button(left_frame, text="Slow Backward", width=25, bg=BC, fg=FG)
-        self.R_turn = tk.Button(left_frame, text="Right Turn", width=25, bg=BC, fg=FG)
-        self.L_turn = tk.Button(left_frame, text="Left Turn", width=25, bg=BC, fg=FG)
-        self.All_stop = tk.Button(left_frame, text="Motor Stop", width=25, bg=BC, fg=FG)
-        self.bind_spot = tk.Button(left_frame, text="Push this button to \n control with W,A,S,D. \n Yellow = Active", width=25, height=5, bg=BC, fg=FG)
+        self.but_send = tk.Button(self, text="Send", width=5, bg=BC, fg=FG)
+        self.mes_entry = tk.Entry(self, width=40)
+        self.clear_but = tk.Button(self, text="Delete text", width=10, bg=BC, fg=FG)
+        self.txt_field = tk.Text(self, width=25, height=22, wrap="word", yscrollcommand=set(), borderwidth=5, padx=3, font="systemfixed")
+        self.F_forward = tk.Button(self, text="Fast Forward", width=25, bg=BC, fg=FG)
+        self.S_forward = tk.Button(self, text="Slow Forward", width=25, bg=BC, fg=FG)
+        self.F_backward = tk.Button(self, text="Fast Backward", width=25, bg=BC, fg=FG)
+        self.S_backward = tk.Button(self, text="Slow Backward", width=25, bg=BC, fg=FG)
+        self.R_turn = tk.Button(self, text="Right Turn", width=25, bg=BC, fg=FG)
+        self.L_turn = tk.Button(self, text="Left Turn", width=25, bg=BC, fg=FG)
+        self.All_stop = tk.Button(self, text="Motor Stop", width=25, bg=BC, fg=FG)
+        self.bind_spot = tk.Button(self, text="Push this button to \n control with W,A,S,D. \n Yellow = Active", width=25, height=5, bg=BC, fg=FG)
 
-        self.but_send.grid(row=0, column=0, pady=3, padx=2)
-        self.mes_entry.grid(row=0, column=1, pady=3, padx=2)
-        self.but_del.grid(row=0, column=2, pady=3, padx=2)
+        self.but_send.grid(row=1, column=0, pady=3, padx=2)
+        self.mes_entry.grid(row=1, column=1, pady=3, padx=2)
+        self.clear_but.grid(row=1, column=2, pady=3, padx=2)
         self.txt_field.grid(rowspan=10, columnspan=3, pady=3, padx=2, sticky="nsew")
-        self.F_forward.grid(row=1,column=4, padx=20,pady=5, sticky="n")
-        self.S_forward.grid(row=2,column=4, padx=20,pady=5, sticky="n")
-        self.F_backward.grid(row=3,column=4, padx=20,pady=5, sticky="n")
-        self.S_backward.grid(row=4,column=4, padx=20,pady=5, sticky="n")
-        self.R_turn.grid(row=5,column=4, padx=20,pady=5, sticky="n")
-        self.L_turn.grid(row=6,column=4, padx=20,pady=5, sticky="n")
-        self.All_stop.grid(row=7,column=4, padx=20,pady=5, sticky="n")
-        self.bind_spot.grid(row=8, column=4,padx=20, pady=15, sticky="n")
+        self.F_forward.grid(row=2,column=3, padx=20,pady=5, sticky="n")
+        self.S_forward.grid(row=3,column=3, padx=20,pady=5, sticky="n")
+        self.F_backward.grid(row=4,column=3, padx=20,pady=5, sticky="n")
+        self.S_backward.grid(row=5,column=3, padx=20,pady=5, sticky="n")
+        self.R_turn.grid(row=6,column=3, padx=20,pady=5, sticky="n")
+        self.L_turn.grid(row=7,column=3, padx=20,pady=5, sticky="n")
+        self.All_stop.grid(row=8,column=3, padx=20,pady=5, sticky="n")
+        self.bind_spot.grid(row=9, column=3,padx=20, pady=15, sticky="n")
 
         self.but_send.bind("<Button-1>", self.printing)
-        self.but_del.bind("<Button-1>", self.printing)
-        self.F_forward.bind("<Button-1>", self.printing)
-        self.S_forward.bind("<Button-1>", self.printing)
-        self.F_backward.bind("<Button-1>", self.printing)
-        self.S_backward.bind("<Button-1>", self.printing)
-        self.R_turn.bind("<Button-1>", self.printing)
-        self.L_turn.bind("<Button-1>", self.printing)
-        self.All_stop.bind("<1>", self.printing)
+        self.clear_but.bind("<Button-1>", self.clear)
+        self.F_forward.bind("<Button-1>", self.forward)
+        self.S_forward.bind("<Button-1>", self.forward)
+        self.F_backward.bind("<Button-1>", self.backward)
+        self.S_backward.bind("<Button-1>", self.backward)
+        self.R_turn.bind("<Button-1>", self.right_turn)
+        self.L_turn.bind("<Button-1>", self.left_turn)
+        self.All_stop.bind("<Button-1>", self.Stop)
 
         self.bind_spot.bind("<w>", self.forward)
         self.bind_spot.bind("<s>", self.backward)
         self.bind_spot.bind("<a>", self.left_turn)
         self.bind_spot.bind("<d>", self.right_turn)
-        #self.bind_spot.bind("<Button-1>", lambda event: self.bind_spot.focus_set())
+        self.bind_spot.bind("<x>", self.Stop)
         self.bind_spot.bind("<Button-1>", self.Button_color)
 
-
-    # ----------------------------------------------------- Right
-        self.right_frame = tk.Frame(self, bg="")
-        self.right_frame.pack()
+        self.PhotoLabel = tk.Label(self, image=self.WALLE)
+        self.PhotoLabel.grid(row=2, rowspan=8, column=4)
 
     def setName_C(self, name):
-        self.name = name
         self.tkraise()
 
     def printing(self, event):      # Include "name" then the username should be included.. later
         #self.name = name
         print("hey")
         self.txtOut = self.mes_entry.get()
-        self.txt_field.insert(0.0, "Controller:" + str(self.txtOut) + "\n")
+        self.txt_field.insert(1000000.0, "Controller:" + str(self.txtOut) + "\n")
         self.mes_entry.delete(0, 'end')
 
     def forward(self, event):
         print("Forward")
-        self.txtOut = self.mes_entry.get()
-        self.txt_field.insert(0.0, str(self.name)+":" + str(self.txtOut) + "\n")
-        self.mes_entry.delete(0, 'end')
+        self.txt_field.insert(100000.0,"Controller: Forward\n")
 
     def backward(self, event):
         print("Backward")
+        self.txt_field.insert(100000.0, "Controller: Backward\n")
 
     def left_turn(self, event):
         print("Left Turn")
+        self.txt_field.insert(1000000.0, "Controller: Left Turn\n")
 
     def right_turn(self, event):
         print("Right Turn")
+        self.txt_field.insert(1000000.0, "Controller: Right Turn\n")
+
+    def Stop(self, event):
+        print("Stop")
+        self.txt_field.insert(10000.0, "Controller: Stop\n")
+
+    def clear(self, event):
+        self.txt_field.delete(1.0, 'end')
 
     def Button_color(self, event):
 
@@ -270,6 +293,8 @@ class RobotControl(tk.Frame):
 class MovieDecider(tk.Frame):
 
     def __init__(self, parent, controller):
+        self.PopCorn = tk.PhotoImage(file="Pics/Popcorn.png")
+
         tk.Frame.__init__(self, parent, bg=Bg_theme)
         label = ttk.Label(self, text="Movie Decider!", font=LARGE_FONT)   #self is root
         label.pack(pady=5, padx=5)
@@ -277,8 +302,8 @@ class MovieDecider(tk.Frame):
 
 root = Page()
 root.title('Multi-Tool')
-width = 1000
-height = 500
+width = 1050
+height = 575
 x = (root.winfo_screenwidth() // 2  ) - (width // 2)
 y = (root.winfo_screenheight() // 2  ) - (height // 2) - 50
 root.geometry('{}x{}+{}+{}'.format(width, height, x, y))  # This will define the size of the window when it opens
